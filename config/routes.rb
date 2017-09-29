62c6376363c31dd697140ac497380fb74606426e
Diogo Lucas <dpflucas@gmail.com>
2017-09-29 13:24:54 +0100

    generate devise views and controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    passwords:          'users/passwords',
+    registrations:      'users/registrations',
+    sessions:           'users/sessions'
+  }
   get 'pages/index'
 
   root 'pages#index'
